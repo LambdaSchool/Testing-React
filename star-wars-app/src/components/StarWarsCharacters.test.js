1e388785fe695f63b934b57b7ca2a4318dc3c6e8
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-27 17:32:05 -0500

    trying to get test to run in terminal

--- star-wars-app/src/components/StarWarsCharacters.test.js
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import { render, fireEvent, wait } from '@testing-library/react';
-import { getData as MockGetData } from '../api';
+import { getData as mockGetData } from '../api';
 import StarWarsCharacters from './StarWarsCharacters';
 
 jest.mock('../api');
+
+test('renders the chracters data, next button and previous button', async () => {
+    mockGetData.mockResolvedValueOnce({ results: [{
+        name: 'Luke Skywalker',
+        height: '172',
+        mass: '77',
+        hair_color: 'blond',
+        skin_color: 'fair',
+    }]})
+
+    const { getByText } = render(<StarWarsCharacters />);
+
+    const nextButton = getByText(/next/i);
+    const prevButton = getByText(/previous/i);
+
+    fireEvent.click(nextButton);
+    fireEvent.click(prevButton);
+    expect(mockGetData).toHaveBeenCalledTimes(1);
+
+    wait(() => expect(getByText(/'Darth'/i)));
+})
