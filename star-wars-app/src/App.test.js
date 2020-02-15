4a3fe30df9ed23ca6795f56ef2650a19d5631993
Veto Ramirez <vetorobert3@gmail.com>
2020-02-15 10:25:05 -0800

    working on get test to work.

--- star-wars-app/src/App.test.js
+++ star-wars-app/src/App.test.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import * as rtl from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import App from './App'
+// import React from 'react';
+// import * as rtl from '@testing-library/react';
+// import '@testing-library/jest-dom/extend-expect';
+// import App from './App'
 
-test('render of new characters', async () => {
-  const wrapper = rtl.render(<App />);
+// test('render of new characters', async () => {
+//   const wrapper = rtl.render(<App />);
 
-  const newPeeps = await wrapper.findAllByAltText(/peep/i);
+//   const newPeeps = await wrapper.findAllByAltText(/peep/i);
 
-  expect(newPeeps[0]).toBeVisible();
-});
+//   expect(newPeeps[0]).toBeVisible();
+// });
+
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import App from "./App";     
+
+test('loads new characters', async () => {
+  const { findAllByAltText } = render(<App />)
+
+  // Click button
+  fireEvent.click(findAllByAltText(/peep/i))
+
+  // Wait for page to update with query text
+  const char = await findAllByAltText(/peep/i)
+  expect(char).toBe(/peep/i)
+})
